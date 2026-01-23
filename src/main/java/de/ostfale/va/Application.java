eb669a812e2a159c64a7f9559d144e23855ba0d2
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2026-01-23 21:43:37 +0100

    Cleaning styles sheets + move to the right location + use one styles file which imports component style sheets

--- src/main/java/de/ostfale/va/Application.java
+++ src/main/java/de/ostfale/va/Application.java
@@ -3,6 +3,7 @@ package de.ostfale.va;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.aura.Aura;
 import com.vaadin.flow.theme.lumo.Lumo;
 import de.ostfale.va.common.UseLogging;
 import org.springframework.boot.SpringApplication;
@@ -11,10 +12,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@StyleSheet(Lumo.STYLESHEET) // Use Aura.STYLESHEET to use Aura instead
+@StyleSheet(Lumo.STYLESHEET)
 @StyleSheet(Lumo.UTILITY_STYLESHEET)
-@StyleSheet("./themes/custom-theme/styles.css")
-@CssImport("./styles/custom-styles.css")
+@StyleSheet("./css/styles.css")
 public class Application implements AppShellConfigurator, UseLogging {
 
     private static final String LOG_MSG_CONFIGURE = "AppShell :: configurePage";
